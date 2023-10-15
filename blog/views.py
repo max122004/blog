6f6175d6831e36595248166f4d9437e1edc7d40f
max122004 <112434411+max122004@users.noreply.github.com>
2023-10-15 09:18:30 +0300

    test - 2

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -30,13 +31,25 @@ def post_share(request, post_id):
     post = get_object_or_404(Post,
                              id=post_id,
                              status=Post.Status.PUBLISHED)
+    sent = False
+
     if request.method == 'POST':
-    # Форма была передана на обработку
+        # Форма была передана на обработку
         form = EmailPostForm(request.POST)
         if form.is_valid():
-        # Поля формы успешно прошли валидацию
+            # Поля формы успешно прошли валидацию
             cd = form.cleaned_data
-        # ... отправить электронное письмо
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'boldirevmax10@gmail.com',
+                      [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
